c2a4d12b4ca8bded9ee942c644a4912a978f6606
Payal Khatri <khatri.p@northeastern.edu>
2024-02-04 19:19:49 -0500

    Adding GET API for getting user details
    Authentication basic auth headers

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,3 +45,46 @@ export const createUser = async (request, response) => {
     response.status(400).send();
   }
 };
+
+export const authorizeAndGetUser = async (request, response) => {
+  try {
+    const authorizationHeader = request.headers.authorization;
+    if (!authorizationHeader) {
+      response.status(401).send();
+      return;
+    }
+    const decodedCredentials = Buffer.from(
+      authorizationHeader.split(" ")[1],
+      "base64"
+    )
+      .toString()
+      .split(":");
+    const username = decodedCredentials[0];
+    const password = decodedCredentials[1];
+    const existingUser = await User.findOne({
+      where: { username: username },
+    });
+    if (!existingUser) {
+      response.status(401).send();
+    }
+    const comparePasswords = await encryptFunction.comparePasswords(
+      password,
+      existingUser.password
+    );
+    if (!comparePasswords) {
+      response.status(401).send();
+      return;
+    }
+    const responseObject = {
+      id: existingUser.id,
+      first_name: existingUser.firstName,
+      last_name: existingUser.lastName,
+      username: existingUser.username,
+      account_created: existingUser.createdAt.toISOString(),
+      account_updated: existingUser.updatedAt.toISOString(),
+    };
+    response.status(200).json(responseObject).send();
+  } catch {
+    response.status(400).send();
+  }
+};
--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ import * as userController from "../controllers/userController.js";
 const router = Router();
 
 router.post("/user", userController.createUser);
+router.get("/user/self", userController.authorizeAndGetUser);
 
 export default router;
