13b38fa0a6f4a6df65a98178eb42a46ab82f0fc4
Payal Khatri <khatri.p@northeastern.edu>
2024-02-19 23:42:54 -0500

    Changing babel config format to json Changing type to module in package.json Installing node modules before enabling systemd service

--- vm-image/systemD/systemdSetup.sh
+++ vm-image/systemD/systemdSetup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-sudo groupadd csye6225
-sudo adduser csye6225 --shell /usr/bin/nologin -g csye6225
-
 sudo cp /tmp/systemdService.service /etc/systemd/system/
 
 sudo systemctl daemon-reload
 sudo systemctl enable systemdService
+
+echo "sleep start"
+
+sleep 20m
